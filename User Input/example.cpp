ebe54c32abc837e4dd37b36b4d827b62a692725e
Nidhi Rajput <rajputnidhi862@gmail.com>
2026-04-10 17:54:17 +0000

    changes

---
+++ User Input/example.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+    int marks;
+    cout<< "Enter my marks:";
+
+    //taking input from user/me
+    cin>> marks;
+    cout<<" my marks are"<< marks;
+
+    return 0;
+}
