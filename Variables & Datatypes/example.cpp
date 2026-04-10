465dbbe4e8a5291ba8ba2b62771db14baec8e8f9
Nidhi Rajput <rajputnidhi862@gmail.com>
2026-04-10 14:10:34 +0000

    setuping of folder

---
+++ Variables & Datatypes/example.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+    //decleration of age
+    int age;
+    cout<<" enter your age : "<< age << endl; // garbage value le lega
+
+    //defination 
+    int mango =10;
+    cout <<"number of mango is: " << mango << endl;
+
+    //updation
+    mango =90;
+    cout<< " number of mango is: " << mango;
+
+
+}
