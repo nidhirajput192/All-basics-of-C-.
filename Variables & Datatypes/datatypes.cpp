0c395a230731f614c122879aed27f9bdb0545f6d
Nidhi Rajput <rajputnidhi862@gmail.com>
2026-04-10 17:14:35 +0000

    variables and datatypes

---
+++ Variables & Datatypes/datatypes.cpp
@@ -0,0 +1,22 @@
+// Datatypes  --->> Build-in or primitive datatypes.....
+ /*
+ 
+ int 
+ float
+ char
+ double
+ boolean
+  
+  */
+ //int
+ int a= 50;
+  
+// char
+char shyam = 'bye';
+
+//double
+double neha = 99.6376;
+
+//boolean -->> true and false, 0 and 1.
+bool gitar= true;
+bool me= 1; 
---
+++ Variables & Datatypes/sizeof.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+    int age =8;
+    cout<< "size of integer :" << sizeof(age);
+    return 0;
+}
+
+
+/* HOW DATA IS GETTING STORED 
+
+Suppose i m having 5 ----->> convert into binary representation
+ int 4 bytes and 1 byte having 8 bits so total ---> 32 bits
+ 00000000 00000000 00000000 00000101*/
